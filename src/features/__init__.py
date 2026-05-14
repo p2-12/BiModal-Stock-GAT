aa1151b92d9c86514d9aa869dfce265f5fd3b783
p2-12 <parinp921@gmail.com>
2026-05-13 20:06:53 -0400

    Refactor legacy daily scorer workflow into modular packages

---
+++ src/features/__init__.py
@@ -0,0 +1,5 @@
+"""Feature engineering modules."""
+
+from .compute import compute_fundamental_features, compute_technical_features
+
+__all__ = ["compute_technical_features", "compute_fundamental_features"]
