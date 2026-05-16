b08377acd6022f444ab416ecf89e0dd98f8b5592
p2-12 <parinp921@gmail.com>
2026-05-16 15:41:03 -0400

    Add backward-compatible feature API exports

---
+++ src/features/__init__.py
@@ -0,0 +1,15 @@
+from .quality import (
+    DEFAULT_RELIABILITY_BY_SOURCE,
+    FeatureQualityConfig,
+    FeatureQualityProcessor,
+    compute_fundamental_features,
+    compute_technical_features,
+)
+
+__all__ = [
+    "DEFAULT_RELIABILITY_BY_SOURCE",
+    "FeatureQualityConfig",
+    "FeatureQualityProcessor",
+    "compute_fundamental_features",
+    "compute_technical_features",
+]
